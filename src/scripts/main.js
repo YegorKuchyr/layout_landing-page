215ab9d677edbae7e393bbccf548a9b3456440ea
YegorSegara <broniga3006@gmail.com>
2026-09-21 19:20:00 +0300

    fix added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,7 @@
 'use strict';
+
+document.querySelectorAll('.menu__link').forEach((link) => {
+  link.addEventListener('click', () => {
+    document.getElementById('menu-toggle').checked = false;
+  });
+});
